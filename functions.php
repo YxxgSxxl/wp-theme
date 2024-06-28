acc1e7fd511fb98672daf14337e509a58fe5b880
YxxgSxxl <114391274+YxxgSxxl@users.noreply.github.com>
2024-06-28 11:39:54 +0200

    add: bootstrap to theme

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+
+function alex_support() {
+    add_theme_support('title-tag');
+}
+
+function alex_register_assets() {
+    wp_enqueue_script( 'bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js', array( 'jquery' ), null, true );
+    wp_enqueue_style( 'bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css');
+
+
+}
+
+add_action('after_setup_theme', 'alex_support');
+add_action('wp_enqueue_scripts', 'alex_register_assets');
